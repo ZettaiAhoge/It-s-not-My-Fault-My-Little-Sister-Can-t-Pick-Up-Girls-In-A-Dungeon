e36148e552a3bb3412c6cbf9fcd7bf1dbb2aa1ef
changsai39 <changsai39@gmail.com>
2018-06-25 20:27:18 -0700

    Pushing more assets

---
+++ Assets/Src/Rpg/DialogueSlice.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueSlice {
+
+    int characterLeft;
+    int characterRight;
+    bool slide;
+    string name;
+    string text;
+    bool pauseForDecision;
+    float lettersPerSecond;
+
+    public DialogueSlice(int characterLeft, int characterRight, bool slide, bool pauseForDecision, float lettersPerSecond, string name, string text)
+    {
+        this.characterLeft = characterLeft;
+        this.characterRight = characterRight;
+        this.slide = slide;
+        this.name = name;
+        this.text = text;
+        this.pauseForDecision = pauseForDecision;
+        this.lettersPerSecond = lettersPerSecond;
+    }
+}

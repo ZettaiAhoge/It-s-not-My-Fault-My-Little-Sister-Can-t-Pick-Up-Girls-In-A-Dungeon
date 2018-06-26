0b0ed3fdb5394b63eddcc4e98208fef4ba8ae817
Adam Kalman <dkeka.zkfaks@gmail.com>
2018-06-25 18:58:21 -0700

    add gitignore

---
+++ Assets/Src/MusicSceneState/MusicData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicData : MonoBehaviour
+{
+    public string musicFile;
+
+    public static void AddToGameObject(GameObject obj, string musicFile)
+    {
+        obj.AddChild();
+    }
+}
